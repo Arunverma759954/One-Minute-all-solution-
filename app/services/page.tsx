61b4240527eade005595f13f52796ebf009b6279
digitalsolution360global <ds360host@gmail.com>
2025-12-04 11:30:10 +0530

    Update page.tsx

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,11 +1,14 @@
-
-"use client";
-
 import ServiceSection from "./ServiceSection";
-import React from "react";
+
+export const metadata = {
+  title: "Our Services | Atithi Homestay – Premium Rooms & Guest Facilities",
+  description:
+    "Explore the services offered by Atithi Homestay including luxury rooms, guest support, modern amenities, hygienic stay, and family-friendly facilities for a comfortable experience.",
+  keywords:
+    "Atithi Homestay services, luxury homestay services, room facilities, guest support, homestay amenities, premium rooms near me, family stay services, homestay Hanamkonda, best guest house services",
+
+ };
 
 export default function Page() {
-  return (
-    <ServiceSection />
-  );
+  return <ServiceSection />;
 }
