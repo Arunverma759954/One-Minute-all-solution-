baf07d1f3fd11295861d69d4cccc39f5a7275180
digitalsolution360global <ds360host@gmail.com>
2025-12-04 11:28:26 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,15 @@
-
-"use client";
-
 import Contactsection from "./ContactSection";
-import React from "react";
+
+export const metadata = {
+  title: "Contact Us | Atithi Homestay – Get in Touch for Bookings & Enquiries",
+  description:
+    "Contact Atithi Homestay for room bookings, stay enquiries, pricing details, and guest support. We're available to assist you with clean, comfortable and premium homestay services.",
+  keywords:
+    "Atithi Homestay contact, homestay booking, rooms near me, contact homestay, guest house booking, Atithi Homestay phone number, stay enquiry, homestay support",
+
+ 
+};
 
 export default function Page() {
-  return (
-    <Contactsection />
-  );
+  return <Contactsection />;
 }
