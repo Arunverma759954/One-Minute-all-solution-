bc705a21494cc6bd75c629c1e6e930e1562581b6
digitalsolution360global <ds360host@gmail.com>
2026-02-13 09:22:11 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -37,26 +37,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.7,
     },
-
-    {
-      url: `${baseUrl}/rooms`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-
-    {
-      url: `${baseUrl}/testimonials`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
-    },
-
-    {
-      url: `${baseUrl}/blog`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
   ]
 }
